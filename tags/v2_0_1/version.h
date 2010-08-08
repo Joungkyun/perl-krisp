6ff60685ca0530a1bab28c82117020a5d405ad97
nobody <nobody@c3c97a0c-65cc-11e0-84c7-af8cc3eece7f>
2010-08-08 16:58:42 +0000

    This commit was manufactured by cvs2svn to create tag 'v2_0_1'.

---
+++ tags/v2_0_1/version.h
@@ -0,0 +1,15 @@
+/*
+ * $Id: version.h,v 1.2 2010-08-02 18:08:18 oops Exp $
+ */
+
+#define MODVER "2.0.1"
+#define MODUVER "002000001"
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
